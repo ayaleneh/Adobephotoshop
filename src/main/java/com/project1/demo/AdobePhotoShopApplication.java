6ddcc628b694fa01408e56fae1fb2805caaec59e
ayaleneh <getaunayaleneh@gmail.com>
2019-02-19 11:40:16 -0500

    Initial commit

---
+++ src/main/java/com/project1/demo/AdobePhotoShopApplication.java
@@ -0,0 +1,13 @@
+package com.project1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdobePhotoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdobePhotoShopApplication.class, args);
+    }
+
+}
